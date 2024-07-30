2c7610b4989cddb7496a3aa1321258c25b71c4df
ValterSantos23 <valterfabriciosantos2018@gmail.com>
2024-07-30 17:33:47 -0300

    Criação do projeto com properties configurado.

---
+++ DesafioWeb/src/main/java/com/web/desafio/DesafioWeb/DesafioWebApplication.java
@@ -0,0 +1,13 @@
+package com.web.desafio.DesafioWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioWebApplication.class, args);
+	}
+
+}
